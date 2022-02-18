30b9c0e50c77e081da8094f91f7183b5aa898062
cjackson-variant <70218217+cjackson-variant@users.noreply.github.com>
2022-02-18 15:44:25 -0500

    Removed feature flag stuff. Updated Messaging library. Updated queue handler & tests.

--- src/TicketType.Microservice.Template/Handlers/EntitySqsQueueHandler.cs
+++ src/TicketType.Microservice.Template/Handlers/EntitySqsQueueHandler.cs
@@ -2,27 +2,20 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Variant.TicketsShared.Messaging.Abstracts;
 using Variant.MessageHandler.MessageHandler;
-using Variant.TicketsShared.Messaging;
 using Variant.TicketsShared.Messaging.Interfaces;
 
 namespace TicketType.Microservice.Template.Handlers
 {
     public class EntitySqsQueueHandler : AbstractSQSHandler
     {
-        private readonly ILogger<IMessageHandler> _logger;
-        private readonly EntityApiChecklist _checklist;
-
         public EntitySqsQueueHandler(
             ILogger<IMessageHandler> logger,
-            EntityApiChecklist checklist
+            IEntityApiChecklist checklist
         ) : base (logger, checklist)
-        {
-            _logger = logger;
-            _checklist = checklist;
-        }
+        {}
 
         // Required by IMessageHandler
-        public async Task Init()
+        public override async Task Init()
         {
             if (_checklist.TractorApiReady)
             {
--- src/TicketType.Microservice.Template/Startup.cs
+++ src/TicketType.Microservice.Template/Startup.cs
@@ -6,13 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TicketType.Microservice.Template.Handlers;
-using TicketType.Microservice.Template.Modules;
-using Variant.MessageHandler.DependencyInjection;
-using Variant.MessageHandler.Epsagon;
-using Variant.MessageHandler.HealthChecks.HealthChecks.AtCapacityWithinWindow;
-using Variant.MessageHandler.HealthChecks.HealthChecks.AverageMessageProcessingDuration;
-using Variant.MessageHandler.HealthChecks.HealthChecks.FailedMessageRatio;
-using Variant.MessageHandler.HealthChecks.HealthChecks.TimeSinceLastBatchReceive;
 using Variant.TicketsShared.Messaging.DependencyInjection;
 
 namespace TicketType.Microservice.Template
@@ -32,29 +25,8 @@ namespace TicketType.Microservice.Template
         public static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
         {
             var config = hostContext.Configuration;
-            var healthCheckConfig = config.GetSection("HealthChecks");
-            var areHealthChecksEnabled = healthCheckConfig.GetValue<bool>("Enabled");
-            // var healthCheckRunnerConfig = healthCheckConfig.GetSection("Runner");
-            // var consoleHealthCheckServerConfig = healthCheckConfig.GetSection("Server");
 
-            void ConfigureMessageConfigurator(IMessageHandlerConfigurator configurator)
-            {
-                configurator.AddEpsagonTracing();
-
-                if (areHealthChecksEnabled)
-                {
-                    configurator
-                        .AddAverageMessageDurationHealthCheck(opts => opts.ProvideDetailedInformation = false)
-                        .AddAtCapacityWithinWindowHealthCheck(opts => opts.ProvideDetailedInformation = false)
-                        .AddFailedMessageRatioHealthCheck(opts => opts.ProvideDetailedInformation = false)
-                        .AddTimeSinceLastBatchReceiveHealthCheck(opts => opts.ProvideDetailedInformation = false);
-                }
-            }
-
-            services.AddFeatureFlags(config)
-                .AddSQSSharedMessaging<EntitySqsQueueHandler>(
-                config);
-
+            services.AddSQSSharedMessaging<EntitySqsQueueHandler>(config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
