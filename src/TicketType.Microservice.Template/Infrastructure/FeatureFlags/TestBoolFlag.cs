546e48141e46a2fffecb3489d83dbaaf31356a32
Marcin Chmielarski <marcin.chmielarski@itmagination.com>
2022-02-22 13:11:04 +0100

    implementation

---
+++ src/TicketType.Microservice.Template/Infrastructure/FeatureFlags/TestBoolFlag.cs
@@ -0,0 +1,12 @@
+﻿using Variant.TicketsShared.LaunchDarklyExtensions;
+
+namespace TicketType.Microservice.Template.Infrastructure.FeatureFlags
+{
+    internal class TestBoolFlag : BooleanFeatureFlag
+    {
+        protected override string FlagId => "ticketing-testflag";
+        public TestBoolFlag(IBooleanFeatureFlagValueProvider valueProvider) : base(valueProvider)
+        {
+        }
+    }
+}
--- src/TicketType.Microservice.Template/Startup.cs
+++ src/TicketType.Microservice.Template/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TicketType.Microservice.Template.Handlers;
+using Variant.TicketsShared.LaunchDarklyExtensions;
 using Variant.TicketsShared.Messaging.DependencyInjection;
 
 namespace TicketType.Microservice.Template
@@ -27,6 +28,15 @@ namespace TicketType.Microservice.Template
             var config = hostContext.Configuration;
 
             services.AddSQSSharedMessaging<EntitySqsQueueHandler>(config);
+
+            var ldSection = config.GetSection("LaunchDarkly");
+            var ldConfig = new LaunchDarklyConfiguration
+            {
+                Key = ldSection.GetValue<string>("Key"),
+                UserName = ldSection.GetValue<string>("UserName")
+            };
+
+            services.AddFeatureFlags(ldConfig);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
