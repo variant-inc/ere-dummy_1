d7d75e7ea58de07ccfd4f919282d0e0bda58e48b
cjackson-variant <70218217+cjackson-variant@users.noreply.github.com>
2022-02-24 14:21:00 -0500

    Updates for deploy. Removed unused dependencies. Downgraded AWSSDK.SecurityToken to 3.7.1.68.

--- src/TicketType.Microservice.Template/Startup.cs
+++ src/TicketType.Microservice.Template/Startup.cs
@@ -1,12 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
 using Epsagon.Dotnet.Instrumentation;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using TicketType.Microservice.Template.Handlers;
-using TicketType.Microservice.Template.Infrastructure.FeatureFlags;
-using Variant.TicketsShared.LaunchDarklyExtensions;
 using Variant.TicketsShared.Messaging.DependencyInjection;
 
 namespace TicketType.Microservice.Template
@@ -30,25 +27,5 @@ namespace TicketType.Microservice.Template
             var logger = provider.GetRequiredService<ILogger>();
             logger.Information("Services Configured!");
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        // {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c =>
-            //         c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketType.Microservice.Template v1"));
-            // }
-            //
-            // app.UseHttpsRedirection();
-            //
-            // app.UseRouting();
-            //
-            // app.UseAuthorization();
-            //
-            // app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        // }
     }
 }
